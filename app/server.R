b613703bc481763b4fd66040905fbeecd3a9a885
rmhirota <re.hirota@gmail.com>
2021-08-04 19:40:09 -0300

    update doc, update app

--- app/server.R
+++ app/server.R
@@ -35,7 +35,69 @@ shinyServer(function(input, output) {
         filterable = TRUE,
         defaultPageSize = 30
       )
+  })
 
+  output$req_votados <- reactable::renderReactable({
+    docsCPI::req_apreciados %>%
+      dplyr::mutate(
+        data_apresentacao = lubridate::dmy(data_apresentacao),
+        n = as.numeric(stringr::str_squish(stringr::str_remove(n, "/ 2021")))
+      ) %>%
+      tidyr::separate(
+        situacao, into = c("situacao", "data_apreciacao"),
+        sep = " Data de apreciação: "
+      ) %>%
+      dplyr::mutate(data_apreciacao = lubridate::dmy(data_apreciacao)) %>%
+      reactable::reactable(
+        columns = list(
+          n = reactable::colDef("N\u00ba", maxWidth = 50),
+          n_link = reactable::colDef(
+            "Download", cell = function(value) {
+              htmltools::tags$a(href = value, target = "_blank", "PDF")
+            },
+            maxWidth = 80
+          ),
+          data_apresentacao = reactable::colDef(
+            "Data de apresentação", maxWidth = 120
+          ),
+          ementa = reactable::colDef("Descrição", minWidth = 200),
+          autoria = reactable::colDef("Autoria"),
+          situacao = reactable::colDef("Situação"),
+          data_apreciacao = reactable::colDef(
+            "Data de apreciação", maxWidth = 120
+          )
+        ),
+        sortable = TRUE,
+        filterable = TRUE,
+        defaultPageSize = 30
+      )
+  })
+  output$req_nao_apreciados <- reactable::renderReactable({
+    docsCPI::req_nao_apreciados %>%
+      dplyr::mutate(
+        data_apresentacao = lubridate::dmy(data_apresentacao),
+        n = as.numeric(stringr::str_squish(stringr::str_remove(n, "/ 2021")))
+      ) %>%
+      reactable::reactable(
+        columns = list(
+          n = reactable::colDef("N\u00ba", maxWidth = 50),
+          n_link = reactable::colDef(
+            "Download", cell = function(value) {
+              htmltools::tags$a(href = value, target = "_blank", "PDF")
+            },
+            maxWidth = 80
+          ),
+          data_apresentacao = reactable::colDef(
+            "Data de apresentação", maxWidth = 120
+          ),
+          ementa = reactable::colDef("Descrição", minWidth = 200),
+          autoria = reactable::colDef("Autoria"),
+          situacao = reactable::colDef("Situação")
+        ),
+        sortable = TRUE,
+        filterable = TRUE,
+        defaultPageSize = 30
+      )
   })
 
 })
--- app/ui.R
+++ app/ui.R
@@ -10,12 +10,13 @@ ui <- dashboardPage(
     dashboardSidebar(
       sidebarMenu(
       id = "sidebarid",
-      menuItem("Documentos", tabName = "page1")
+      menuItem("Documentos recebidos", tabName = "page1"),
+      menuItem("Requerimentos votados", tabName = "page2"),
+      menuItem("Requerimentos não apreciados", tabName = "page3")
     )),
 
     # body ----
     dashboardBody(tabItems(
-
       tabItem(
         tabName = "page1",
         fluidRow(
@@ -24,6 +25,30 @@ ui <- dashboardPage(
             id = "docs",
             title = "Documentos recebidos",
             reactable::reactableOutput("docs")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "page2",
+        fluidRow(
+          box(
+            width = 12,
+            id = "docs",
+            title = "Requerimentos votados",
+            reactable::reactableOutput("req_votados") %>%
+              shinycssloaders::withSpinner(type = 8, color = "#444444")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "page3",
+        fluidRow(
+          box(
+            width = 12,
+            id = "docs",
+            title = "Requerimentos não apreciados",
+            reactable::reactableOutput("req_nao_apreciados") %>%
+              shinycssloaders::withSpinner(type = 8, color = "#444444")
           )
         )
       )
